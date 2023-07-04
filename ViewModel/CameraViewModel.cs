c78309250c80f4c21854fbc6c58f8d06a764706c
= <=>
2023-07-04 16:09:47 +0700

    sync

--- ViewModel/CameraViewModel.cs
+++ ViewModel/CameraViewModel.cs
@@ -3,23 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Basler.Pylon;
 
 namespace BaslerCameraVisual.ViewModel
 {
-    public class CameraViewModel : BaseViewModel.BaseViewModel
+    public class CameraViewModel:BaseViewModel.BaseViewModel
     {
         public CameraViewModel()
         {
-
-        }
-
-        private void Opencam() 
-        {
-            using(Camera camera= new Camera())
-            {
-            
-            }
+          
         }
     }
 }
