8cc7346e0b9ce016353dc1e966dcf72a90274202
= <=>
2023-07-04 14:46:17 +0700

    fist commit

---
+++ ViewModel/CameraViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaslerCameraVisual.ViewModel
+{
+    public class CameraViewModel:BaseViewModel.BaseViewModel
+    {
+        public CameraViewModel()
+        {
+          
+        }
+    }
+}
