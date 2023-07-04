95960b7bdf6e047eae1a5fb1714132f3a06cbc71
Quý Thi Bùi Nguyễn <quythi.buinguyen@gmail.com>
2023-07-04 16:14:36 +0700

    sync1

--- ViewModel/CameraViewModel.cs
+++ ViewModel/CameraViewModel.cs
@@ -3,14 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Basler.Pylon;
 
 namespace BaslerCameraVisual.ViewModel
 {
-    public class CameraViewModel:BaseViewModel.BaseViewModel
+    public class CameraViewModel : BaseViewModel.BaseViewModel
     {
         public CameraViewModel()
         {
-          
+
+        }
+
+        private void Opencam() 
+        {
+            using(Camera camera= new Camera())
+            {
+            
+            }
         }
     }
 }
